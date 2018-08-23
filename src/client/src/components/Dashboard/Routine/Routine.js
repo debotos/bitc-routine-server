3e852883cd563f2e78e4a9cad328b59bd45e1fac
Debotos Das <debotosdas@gmail.com>
2018-08-23 19:38:07 +0600

    added markup of a single cell

--- src/client/src/components/Dashboard/Routine/Routine.js
+++ src/client/src/components/Dashboard/Routine/Routine.js
@@ -1,53 +1,53 @@
 import React, { Component } from 'react';
-import { Container, Columns, Column, Box } from 'bloomer';
-import { Table } from 'react-data-components-bulma';
+import { Container, Columns, Column, Box, Table, Button } from 'bloomer';
 import { connect } from 'react-redux';
 
 import { clearErrors } from '../../../redux/actions/profileActions';
 
 import './routine.css';
 import AddPeriod from './AddPeriod';
+import SingleCell from './SingleCell';
 
 class Routine extends Component {
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.routine) {
-      console.log('Before', this.state.columns);
-      this.setState({ columns: this.generateColumn(nextProps.routine) }, () => {
-        console.log('After', this.state.columns);
-      });
-    }
-  }
-  componentWillMount = () => {
-    for (let i = 0; i < this.state.days.length; i++) {
-      this.state.RoutineData.push({
-        id: i,
-        day: this.state.days[i]
-      });
-    }
-  };
-  handlePeriodChange = data => {
-    console.log(data);
-  };
-
-  // addColumn = () => {
-  //   console.log('Adding a new column ...');
-  //   // Add a new column
-  //   let newColumnData = {
-  //     title: 'time range',
-  //     prop: 'period'
-  //   };
-  //   let currentColumns = this.state.columns;
-  //   console.log('Old Columns ', currentColumns);
-  //   let updatedColumns = [...currentColumns, newColumnData];
-  //   console.log('New Columns ', updatedColumns);
-  //   this.setState({ columns: updatedColumns });
-  // };
   generateColumn = (argRoutine = null) => {
-    let alwaysAlwaysHave = { title: 'Day', prop: 'day' };
+    let alwaysAlwaysHave = (
+      <p>
+        <span>Day</span>
+      </p>
+    );
     if (!argRoutine) {
       let routine = this.props.routine
-        ? this.props.routine.map(singleItem => {
-            return { title: singleItem.period, prop: singleItem.period };
+        ? this.props.routine.map((singleItem, index) => {
+            return (
+              <p
+                key={index}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <span>{singleItem.period}</span>
+                <span
+                  style={{
+                    display: 'flex'
+                  }}
+                >
+                  <Button
+                    isColor="info"
+                    isOutlined
+                    isSize="small"
+                    style={{ marginRight: '5px' }}
+                  >
+                    Edit
+                  </Button>
+                  <Button isColor="danger" isOutlined isSize="small">
+                    <span className="icon is-small">
+                      <i className="fas fa-trash" />
+                    </span>
+                  </Button>
+                </span>
+              </p>
+            );
           })
         : [];
       if (this.props.routine) {
@@ -56,8 +56,34 @@ class Routine extends Component {
       return [alwaysAlwaysHave];
     } else {
       let routine = argRoutine
-        ? argRoutine.map(singleItem => {
-            return { title: singleItem.period, prop: singleItem.period };
+        ? argRoutine.map((singleItem, index) => {
+            return (
+              <p
+                key={index}
+                style={{ display: 'flex', justifyContent: 'space-between' }}
+              >
+                <span>{singleItem.period}</span>
+                <span
+                  style={{
+                    display: 'flex'
+                  }}
+                >
+                  <Button
+                    isColor="info"
+                    isOutlined
+                    isSize="small"
+                    style={{ marginRight: '5px' }}
+                  >
+                    Edit
+                  </Button>
+                  <Button isColor="danger" isOutlined isSize="small">
+                    <span className="icon is-small">
+                      <i className="fas fa-trash" />
+                    </span>
+                  </Button>
+                </span>
+              </p>
+            );
           })
         : [];
       if (argRoutine) {
@@ -66,14 +92,50 @@ class Routine extends Component {
       return [alwaysAlwaysHave];
     }
   };
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.routine) {
+      console.log('Before', this.state.columns);
+      this.setState({ columns: this.generateColumn(nextProps.routine) }, () => {
+        console.log('After', this.state.columns);
+      });
+    }
+  }
+
   state = {
     days: ['Sat', 'Sun', 'Mon', 'Tues', 'Wed', 'Thu'], // This is not going to change [const]
     columns: this.generateColumn(),
     // periods: [], // @todo
-    RoutineData: []
+    RoutineData: [],
+    sat: [
+      {
+        day: 'saturday',
+        classes: [
+          {
+            subject: {
+              title: 'Compailer Design',
+              code: '023'
+            },
+            teacher: {
+              guest: true,
+              name: 'Majida Mam',
+              code: 'MM'
+            },
+            _id: '5b7e7d284c30dc0523562013',
+            semester: 'CSE- 7<sup>th</sup>'
+          }
+        ]
+      }
+    ],
+    sun: [],
+    mon: [],
+    tues: [],
+    wed: [],
+    thu: []
   };
+
   render() {
-    // console.log(this.props.routine);
+    // console.log(this.props.semester);
+    let { days, sat, sun, mon, tues, wed, thu } = this.state;
     return (
       <Container className="routine-container" isFluid>
         <Box style={{}}>
@@ -82,10 +144,54 @@ class Routine extends Component {
               <Table
                 className="table is-bordered is-narrow is-hoverable routine-table"
                 style={{ fontSize: '14px' }}
-                keys="id"
-                dataArray={this.state.RoutineData}
-                columns={this.state.columns}
-              />{' '}
+              >
+                <thead>
+                  <tr>
+                    {/* generating columns */}
+                    {this.state.columns.map((singleColumn, index) => (
+                      <th key={index}>{singleColumn}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{days[0]}</td>
+                    {sat.map((singleCellData, index) => (
+                      <SingleCell data={singleCellData} key={index} />
+                    ))}
+                  </tr>
+                  <tr>
+                    <td>{days[1]}</td>
+                    {sun.map((singleCell, index) => (
+                      <td key={index}>{singleCell}</td>
+                    ))}
+                  </tr>
+                  <tr>
+                    <td>{days[2]}</td>
+                    {mon.map((singleCell, index) => (
+                      <td key={index}>{singleCell}</td>
+                    ))}
+                  </tr>
+                  <tr>
+                    <td>{days[3]}</td>
+                    {tues.map((singleCell, index) => (
+                      <td key={index}>{singleCell}</td>
+                    ))}
+                  </tr>
+                  <tr>
+                    <td>{days[4]}</td>
+                    {wed.map((singleCell, index) => (
+                      <td key={index}>{singleCell}</td>
+                    ))}
+                  </tr>
+                  <tr>
+                    <td>{days[5]}</td>
+                    {thu.map((singleCell, index) => (
+                      <td key={index}>{singleCell}</td>
+                    ))}
+                  </tr>
+                </tbody>
+              </Table>
             </Column>
           </Columns>
         </Box>
@@ -100,6 +206,7 @@ class Routine extends Component {
 const mapStateToProps = state => {
   return {
     routine: state.routine.routineArray
+    // semesters: state.semesters.semesterArray
   };
 };
 export default connect(mapStateToProps, { clearErrors })(Routine);
