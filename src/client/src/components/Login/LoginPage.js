6887b6cb69ee1d500070253ce8965f0b4b265f15
Debotos Das <debotosdas@gmail.com>
2018-08-26 14:21:23 +0600

    for now i am done with pdf

--- src/client/src/components/Login/LoginPage.js
+++ src/client/src/components/Login/LoginPage.js
@@ -49,86 +49,83 @@ class LoginPage extends Component {
   render() {
     const { errors } = this.state;
     return (
-      <section
-        style={{
-          background: '#F2F6FA',
-          fontSize: '14px',
-          fontWeight: 400
-        }}
-        className="hero is-fullheight"
-      >
-        <div className="hero-body">
-          <div className="container has-text-centered">
-            <div className="column is-4 is-offset-4">
-              <h3 className="title has-text-grey">Login</h3>
-              <p className="subtitle has-text-grey">Please login to proceed.</p>
-              <div className="box">
-                <form onSubmit={this.onSubmit}>
-                  <div className="field">
-                    <div className="control">
-                      <input
-                        style={{ fontWeight: 500 }}
-                        name="email"
-                        className="input is-large"
-                        type="email"
-                        placeholder="Your Email"
-                        autoFocus=""
-                        value={this.state.email}
-                        onChange={this.onChange}
-                      />
+      <div style={{ backgroundColor: '#F2F6FA', marginTop: '-3.5rem' }}>
+        <section className="hero is-fullheight">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <div className="column is-4 is-offset-4">
+                <h3 className="title has-text-grey">Login</h3>
+                <p className="subtitle has-text-grey">
+                  Please login to proceed.
+                </p>
+                <div className="box">
+                  <form onSubmit={this.onSubmit}>
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          style={{ fontWeight: 500 }}
+                          name="email"
+                          className="input is-large"
+                          type="email"
+                          placeholder="Your Email"
+                          autoFocus=""
+                          value={this.state.email}
+                          onChange={this.onChange}
+                        />
+                      </div>
+                      <Help style={{ fontWeight: 500 }} isColor="danger">
+                        {errors.email}
+                      </Help>
                     </div>
-                    <Help style={{ fontWeight: 500 }} isColor="danger">
-                      {errors.email}
-                    </Help>
-                  </div>
 
-                  <div className="field">
-                    <div className="control">
-                      <input
-                        style={{ fontWeight: 500 }}
-                        name="password"
-                        className="input is-large"
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.onChange}
-                        placeholder="Your Password"
-                      />
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          style={{ fontWeight: 500 }}
+                          name="password"
+                          className="input is-large"
+                          type="password"
+                          value={this.state.password}
+                          onChange={this.onChange}
+                          placeholder="Your Password"
+                        />
+                      </div>
+                      <Help style={{ fontWeight: 500 }} isColor="danger">
+                        {errors.password}
+                      </Help>
                     </div>
-                    <Help style={{ fontWeight: 500 }} isColor="danger">
-                      {errors.password}
-                    </Help>
-                  </div>
-                  <Button
-                    style={{ marginTop: '1.2rem', width: '100%' }}
-                    isColor="info"
-                    isSize="large"
-                    type="submit"
+                    <Button
+                      style={{ marginTop: '1.2rem', width: '100%' }}
+                      isColor="info"
+                      isSize="large"
+                      type="submit"
+                    >
+                      Login
+                    </Button>
+                  </form>
+                </div>
+                <p className="has-text-grey">
+                  <a
+                    rel="noopener noreferrer"
+                    href="https://www.facebook.com/debotos.das.1997"
+                    target="_blank"
                   >
-                    Login
-                  </Button>
-                </form>
+                    Forgot Password?
+                  </a>{' '}
+                  &nbsp;&nbsp;
+                  <a
+                    rel="noopener noreferrer"
+                    href="https://www.facebook.com/debotos.das.1997"
+                    target="_blank"
+                  >
+                    Need Help?
+                  </a>
+                </p>
               </div>
-              <p className="has-text-grey">
-                <a
-                  rel="noopener noreferrer"
-                  href="https://www.facebook.com/debotos.das.1997"
-                  target="_blank"
-                >
-                  Forgot Password?
-                </a>{' '}
-                &nbsp;&nbsp;
-                <a
-                  rel="noopener noreferrer"
-                  href="https://www.facebook.com/debotos.das.1997"
-                  target="_blank"
-                >
-                  Need Help?
-                </a>
-              </p>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     );
   }
 }
